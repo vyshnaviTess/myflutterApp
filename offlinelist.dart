8135ec4bb73a9a7764a61304f4e68e3989d5fc63
vyshnaviTess <36623168+vyshnaviTess@users.noreply.github.com>
2021-12-07 09:38:46 +0000

    Delete offlinelist.dart

--- offlinelist.dart
+++
@@ -1,7 +0,0 @@
-import 'package:myflutterapp/model/currency.dart';
-import 'package:myflutterapp/datalist/datalist.dart';
-
-//cache data
-abstract class LocalDataRates implements CurrencyRates {
-  Future<void> setRates(List<GbpCurrency> newRates);
-}
